4990e245a9848f9615ab9f761fb4ec40e33827d5
Julien Maupetit <julien@tailordev.fr>
2016-03-04 18:15:25 +0100

    Add a draft layout

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export default () => <div>Hello, World!</div>;
+var App = React.createClass({
+  render: function() {
+    return (
+        <div className="layout">
+          <header className="page-header">
+            <h1>TDit <small>TailorDev Markdown Editor</small></h1>
+          </header>
+          <div className="content">
+            <div className="editor">
+              Edit here.
+            </div>
+            <div className="preview">
+              Preview here.
+            </div>
+          </div>
+        </div>
+    );
+  }
+});
+
+export default App;
