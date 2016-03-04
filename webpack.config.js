4c74f7e93587e82ec84902d34e547a589ede8882
William Durand <will+git@drnd.me>
2016-03-04 13:42:58 +0100

    Add setup files

---
+++ webpack.config.js
@@ -0,0 +1,128 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const pkg = require('./package.json');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    // Add resolve.extensions.
+    // '' is needed to allow imports without an extension.
+    // Note the .'s before extensions as it will fail to match without!!!
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['eslint'],
+                include: PATHS.app
+            }
+        ],
+        loaders: [
+            {
+                // Test expects a RegExp! Note the slashes!
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths.
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                // Enable caching for improved performance during development
+                // It uses default OS directory by default. If you need something
+                // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+                loaders: ['babel?cacheDirectory'],
+                // Parse only app files! Without this it will go through entire project.
+                // In addition to being slow, that will most likely result in an error.
+                include: PATHS.app
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'node_modules/html-webpack-template/index.ejs',
+            title: pkg.name,
+            appMountId: 'app',
+            inject: false
+        })
+    ]
+};
+
+// Default configuration
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+
+            host: process.env.HOST || '127.0.0.1',
+            port: process.env.PORT || 8080
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {
+        entry: {
+            vendor: Object.keys(pkg.dependencies).filter(function (v) {
+                // Exclude alt-utils as it won't work with this setup
+                // due to the way the package has been designed
+                // (no package.json main).
+                return v !== 'alt-utils';
+            })
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].[chunkhash].js',
+            chunkFilename: '[chunkhash].js'
+        },
+        plugins: [
+            new CleanPlugin([ PATHS.build ]),
+            // Setting DefinePlugin affects React library size!
+            // DefinePlugin replaces content "as is" so we need some extra
+            // quotes for the generated code to make sense
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest']
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
+}
