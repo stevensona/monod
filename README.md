60e7c7645f434d215c5cde2232e8e7ecffad3f7b
William Durand <will+git@drnd.me>
2016-03-05 19:34:32 +0100

    Add some documentation

--- README.md
+++ README.md
@@ -10,19 +10,42 @@ $ npm install
 $ npm run dev
 ```
 
+#### Common Tasks
 
-## Common Tasks
-
-### `npm run dev`
+##### `npm run dev`
 
 Runs the development server at [`http://localhost:8080`](http://127.0.0.1:8080),
 and use Hot Module Replacement. You can override the default host and port
 through env (`HOST`, `PORT`).
 
-### `npm run build`
+##### `npm run build`
 
 Builds the project in `build/` (production ready).
 
-### `npm run lint`
+##### `npm run lint`
 
 Runs [ESLint](http://eslint.org/).
+
+#### Tools
+
+##### Babel
+
+[Babel](https://babeljs.io/) is modular and distributed in different npm
+modules. The core functionality is available in the `babel-core` npm package,
+the integration with webpack is available through the `babel-loader` npm
+package, and for every type of feature and extensions you want to make available
+to your code, you will need to install a separate package (the most common are
+`babel-preset-es2015` and `babel-preset-react`, for compiling ES6 and React's
+JSX, respectively). A preset is a npm module exporting Babel configuration.
+
+##### Stylesheets
+
+Webpack provides two loaders to deal with stylesheets:
+
+* `css-loader`, which looks for `@import` and `url` statements and resolves
+  them;
+* `style-loader`, which adds all the computed style rules into the page.
+
+Combined together, these loaders enable you to embed stylesheets into a Webpack
+JavaScript bundle. By default, the CSS rules will be bundled together with the
+JavaScript file.
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,47 @@
-const path = require('path');
-const merge = require('webpack-merge');
+const path    = require('path');
+const merge   = require('webpack-merge');
 const webpack = require('webpack');
 
+// Webpack plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 
+// Read `package.json` file
 const pkg = require('./package.json');
 
+// Define some constants
 const TARGET = process.env.npm_lifecycle_event;
-const PATHS = {
+const PATHS  = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
+// Used to configure Babel (see: `.babelrc` file)
 process.env.BABEL_ENV = TARGET;
 
+// Common config, shared by all "targets"
 const common = {
+    // Entry points are used to define "bundles"
     entry: {
         app: PATHS.app
     },
-    // Add resolve.extensions.
-    // '' is needed to allow imports without an extension.
-    // Note the .'s before extensions as it will fail to match without!!!
+    // Extensions that should be used to resolve module
+    //
+    // - `''` is needed to allow imports without an extension
+    // - note the `.` before extensions as it will fail to match without!
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
+    // Tells Webpack how to write the compiled files to disk
+    // Note, that while there can be multiple entry points, only one output
+    // configuration is specified
     output: {
         path: PATHS.build,
+        // `[name]` is replaced by the name of the chunk
         filename: '[name].js'
     },
     module: {
+        // Loaders that run *before* others loaders
         preLoaders: [
             {
                 test: /\.jsx?$/,
@@ -37,29 +49,44 @@ const common = {
                 include: PATHS.app
             }
         ],
+        // Loaders are transformations that are applied on a resource file of
+        // an application
         loaders: [
             {
                 test: /\.scss$/,
+                // Loaders are applied from right to left
                 loaders: ['style', 'css', 'sass'],
                 include: PATHS.app
             },
             {
                 test: /\.jsx?$/,
                 // Enable caching for improved performance during development
-                // It uses default OS directory by default. If you need something
-                // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+                // It uses default OS directory by default. Future webpack
+                // builds will attempt to read from the cache to avoid needing
+                // to run the potentially expensive Babel recompilation process
+                // on each run.
                 loaders: ['babel?cacheDirectory'],
-                // Parse only app files! Without this it will go through entire project.
-                // In addition to being slow, that will most likely result in an error.
+                // Parse only app files! Without this it will go through entire
+                // project. In addition to being slow, that will most likely
+                // result in an error.
                 include: PATHS.app
             }
         ]
     },
+    // Plugins do not operate on individual source files: they influence the
+    // build process as a whole
     plugins: [
+        // Add any given string to the top of the generated bundle file
+        new webpack.BannerPlugin('By the good folks at TailorDev SAS'),
+        // Generate the final HTML5 file, nd include all your webpack bundles
         new HtmlWebpackPlugin({
+            // Here, we use the `html-webpack-template` npm package
             template: 'node_modules/html-webpack-template/index.ejs',
+            // The page's title is read from npm's `package.json` file
             title: pkg.name,
+            // Main "div" `id`
             appMountId: 'app',
+            // ???
             inject: false
         })
     ]
@@ -68,19 +95,24 @@ const common = {
 // Default configuration
 if (TARGET === 'dev' || !TARGET) {
     module.exports = merge(common, {
+        // Enable sourcemaps
         devtool: 'eval-source-map',
+        // Development server + Hot Module Replacement
         devServer: {
-            // Enable history API fallback so HTML5 History API based
-            // routing works. This is a good default that will come
-            // in handy in more complicated setups.
+            // Enable history API fallback so HTML5 History API based routing
+            // works. This is a good default that will come in handy in more
+            // complicated setups.
             historyApiFallback: true,
+            // Enable hot code replacement
             hot: true,
+            // Let Webpack generate the client portion used to connect the
+            // generated bundle running in-memory to the development server
             inline: true,
+            // ???
             progress: true,
-
             // Display only errors to reduce the amount of output.
             stats: 'errors-only',
-
+            // Development server settings
             host: process.env.HOST || '127.0.0.1',
             port: process.env.PORT || 8080
         },
@@ -90,34 +122,43 @@ if (TARGET === 'dev' || !TARGET) {
     });
 }
 
+// Build for production
 if (TARGET === 'build') {
     module.exports = merge(common, {
+        // Tell Webpack that we want a separate entry chunk for our project
+        // `vendor` level dependencies
         entry: {
-            vendor: Object.keys(pkg.dependencies).filter(function (v) {
-                // Exclude alt-utils as it won't work with this setup
-                // due to the way the package has been designed
-                // (no package.json main).
-                return v !== 'alt-utils';
-            })
+            // From npm's `package.json` file
+            vendor: Object.keys(pkg.dependencies)
         },
         output: {
             path: PATHS.build,
+            // Set up caching by adding cache busting hashes to filenames
+            // `[chunkhash]` returns a chunk specific hash
             filename: '[name].[chunkhash].js',
+            // The filename of non-entry chunks
             chunkFilename: '[chunkhash].js'
         },
         plugins: [
+            // `rm -rf`
             new CleanPlugin([ PATHS.build ]),
             // Setting DefinePlugin affects React library size!
-            // DefinePlugin replaces content 'as is' so we need some extra
+            // DefinePlugin replaces content "as is" so we need some extra
             // quotes for the generated code to make sense
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': '"production"'
             }),
+            // Allow to extract the code we need for the `vendor` bundle,
+            // otherwise `app.js` will still contains `vendor` dependencies
             new webpack.optimize.CommonsChunkPlugin({
+                // Extract vendor and manifest files, the latter being a file
+                // that tells Webpack how to map each module to each file
                 names: ['vendor', 'manifest']
             }),
+            // Minification with Uglify
             new webpack.optimize.UglifyJsPlugin({
                 compress: {
+                    // Ignore warning messages are they are pretty useless
                     warnings: false
                 }
             })
