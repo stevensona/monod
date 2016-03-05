e99a567c46a2dea5c7e2c63be3855a86a3f9b722
William Durand <will+git@drnd.me>
2016-03-05 22:35:21 +0100

    Rewrite App component with ES6 class

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-var App = React.createClass({
-  render: function() {
+export default class App extends Component {
+  render() {
     return (
-        <div className="layout">
-          <header className="page-header">
-            <h1>TDit <small>TailorDev Markdown Editor</small></h1>
-          </header>
-          <div className="content">
-            <div className="editor">
-              Edit here.
-            </div>
-            <div className="preview">
-              Preview here.
-            </div>
+      <div className="layout">
+        <header className="page-header">
+          <h1>TDit <small>TailorDev Markdown Editor</small></h1>
+        </header>
+        <div className="content">
+          <div className="editor">
+            Edit here.
+          </div>
+          <div className="preview">
+            Preview here.
           </div>
         </div>
+      </div>
     );
   }
-});
-
-export default App;
+}
