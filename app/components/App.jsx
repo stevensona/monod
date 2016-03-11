ad6322698d4a194a3fae4cd535d68ab152940c35
William Durand <will+git@drnd.me>
2016-03-11 17:13:28 +0100

    Add default content (the dirty way)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -21,8 +21,26 @@ export default class App extends Component {
 
   loadRaw() {
     return localforage.getItem(this.forageKey).then((value) => {
-      return null !== value ? value : '';
-    });
+      return null !== value ? value : [
+        'Introducing Monod',
+        '=================',
+        '',
+        '> **TL;DR** This editor is the first experiment we wanted to tackle at **Le lab**. This _week #1 release_ is a pure client-side application written with [React](https://facebook.github.io/react/) by the good folks at [TailorDev](https://tailordev.fr)!',
+        '',
+        'Read more about how and why we build Monod at: https://tailordev.fr/blog/.',
+        '',
+        '### Markdown syntax',
+        '',
+        'This note demonstrates some of what [Markdown][1] is capable of doing.',
+        '',
+        '``` python',
+        'def hello():',
+        '    print("Have fun with Monod!")',
+        '```',
+        '',
+        '*Play with this page and [give us feedback](mailto:hello@tailordev.fr). We would :heart: to ear from you!*'
+      ].join('\n');
+    })
   }
 
   saveRaw(raw) {
