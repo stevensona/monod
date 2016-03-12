3f154f29db9183e46de6903884faaf6e2df6cc1a
Julien Maupetit <julien@tailordev.fr>
2016-03-12 11:40:06 +0100

    Fix typo

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => (
   <footer className="main">
-    By the good folks <a href="https://tailordev.fr" title="Read more about us" target="_blank">TailorDev</a>, 2016.
+    By the good folks at <a href="https://tailordev.fr" title="Read more about us" target="_blank">TailorDev</a>, 2016.
   </footer>
 )
