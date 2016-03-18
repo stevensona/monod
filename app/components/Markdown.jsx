5804a27427c940acd4a101e592c3dd7c5e0ee3da
Julien Maupetit <julien@tailordev.fr>
2016-03-18 11:07:40 +0100

    Remove the ReactCodeMirror component
    The Markdown component now wraps the CodeMirror instance.

--- app/components/Markdown.jsx
+++ app/components/Markdown.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import MarkdownLoader from './loaders/Markdown';
-import Codemirror from 'react-codemirror';
+import CodeMirror from 'codemirror';
 
 import 'codemirror/lib/codemirror.css';
 
@@ -14,21 +14,9 @@ export default class Markdown extends Component {
     });
   }
 
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  getCodeMirror() {
-    return this.refs.markdownTextarea.getCodeMirror();
-  }
-
-  handleOnScroll() {
-    const { top, height, clientHeight } = this.getCodeMirror().getScrollInfo();
-    this.props.doUpdatePosition(top / (height - clientHeight));
-  }
-
-  render() {
-    var options = {
+  componentDidMount() {
+    const textareaNode = this.refs.markdownTextarea;
+    const options = {
       autofocus: true,
       lineNumbers: true,
       lineWrapping: true,
@@ -37,14 +25,45 @@ export default class Markdown extends Component {
       theme: 'monod'
     };
 
+    // CodeMirror main instance
+    this.codeMirror = CodeMirror.fromTextArea(textareaNode, options);
+
+    // Bind CodeMirror events
+    this.codeMirror.on('change', this.handleOnChange.bind(this));
+
+    // Set default value
+    const defaultValue = this.props.raw || '';
+    this.codeMirror.setValue(defaultValue);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  getCodeMirror() {
+    return this.codeMirror;
+  }
+
+  handleOnChange() {
+    const newValue = this.getCodeMirror().getDoc().getValue();
+    this.props.onChange && this.props.onChange(newValue);
+  }
+
+  handleOnScroll() {
+    const { top, height, clientHeight } = this.getCodeMirror().getScrollInfo();
+    this.props.doUpdatePosition(top / (height - clientHeight));
+  }
+
+  render() {
+
     return (
       <div className="markdown" onScroll={this.handleOnScroll.bind(this)}>
-        <Codemirror
+        <textarea
           ref="markdownTextarea"
           placeholder="Type your *markdown* content here"
           onChange={this.props.onChange}
           value={this.props.raw}
-          options={options}
+          autoComplete="off"
         />
       </div>
     );
