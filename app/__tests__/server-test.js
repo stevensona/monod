2c2bae9d831ff77ad2ab5e213b9e2f0ebb6ca42a
William Durand <will+git@drnd.me>
2016-03-30 17:06:31 +0200

    Implement basic API

---
+++ app/__tests__/server-test.js
@@ -0,0 +1,128 @@
+import request from 'supertest';
+import path from 'path';
+import server from '../server';
+import fs from 'fs';
+
+// see: https://github.com/mochajs/mocha/issues/1847
+const { describe, it } = global;
+
+describe('Express app', () => {
+  const DOCUMENTS_ENDPOINT     = '/documents';
+  const VALID_DOCUMENT_UUID    = '9950e80b-f214-45d0-a98c-bffee2582c71';
+  const NEW_DOCUMENT_UUID      = '0000e80b-f214-45d0-a98c-bffee2581234';
+  const EXISTING_DOCUMENT_UUID = '1111e81b-f214-45d1-a98c-bffee2581234';
+
+  const api = request.agent(server);
+
+  it('calls /', (done) => {
+    api
+      .get('/')
+      .expect('Content-type',/html/)
+      .expect(200, done);
+  });
+
+  describe('- GET /documents/:uuid', (done) => {
+    it('returns 400 if document id is not a valid UUID', (done) => {
+      api
+        .get(`${DOCUMENTS_ENDPOINT}/123`)
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    });
+
+    it('returns an existing document in JSON', (done) => {
+      api
+        .get(`${DOCUMENTS_ENDPOINT}/${VALID_DOCUMENT_UUID}`)
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          content: { ba: 'bar' },
+          last_modified: '2016-03-30T10:50:09.229Z',
+          uuid: '9950e80b-f214-45d0-a98c-bffee2582c71',
+          version: 42
+        }, done);
+    });
+  });
+
+  describe('- PUT /documents/:uuid', (done) => {
+    before((done) => {
+      // remove file created when testing the creation of a new document
+      fs.unlink(path.join(process.env.MONOD_DATA_DIR, NEW_DOCUMENT_UUID), (err) => {
+        // create existing file that will be updated
+        fs.writeFile(
+          path.join(process.env.MONOD_DATA_DIR, EXISTING_DOCUMENT_UUID),
+          '{ "version": 123 }',
+          (err) => {
+            done();
+          }
+        );
+      });
+    });
+
+    after((done) => {
+      // cleanup everything
+      fs.unlink(path.join(process.env.MONOD_DATA_DIR, NEW_DOCUMENT_UUID), (err) => {
+        fs.unlink(path.join(process.env.MONOD_DATA_DIR, EXISTING_DOCUMENT_UUID), (err) => {
+          done();
+        });
+      });
+    });
+
+    it('returns 400 if document id is not a valid UUID', (done) => {
+      api
+        .put(`${DOCUMENTS_ENDPOINT}/123`)
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    });
+
+    it('returns 400 if content is not supplied', (done) => {
+      api
+        .put(`${DOCUMENTS_ENDPOINT}/${NEW_DOCUMENT_UUID}`)
+        .send({})
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    });
+
+    it('returns 201 when a new document is persisted', (done) => {
+      const content = 'foo';
+
+      api
+        .put(`${DOCUMENTS_ENDPOINT}/${NEW_DOCUMENT_UUID}`)
+        .send({ content: content })
+        .expect('Content-Type', /json/)
+        .expect((res) => {
+          // fix date value, given by the server, but ensure it is part of the
+          // response
+          if (res.body.last_modified) {
+            res.body.last_modified = 'date';
+          }
+        })
+        .expect(201, {
+          content: content,
+          uuid: NEW_DOCUMENT_UUID,
+          last_modified: 'date',
+          version: 1
+        }, done);
+    });
+
+    it('returns 200 when an existing document is updated', (done) => {
+      const content = 'bar';
+
+      api
+        .put(`${DOCUMENTS_ENDPOINT}/${EXISTING_DOCUMENT_UUID}`)
+        .send({ content: content })
+        .expect('Content-Type', /json/)
+        .expect((res) => {
+          // fix date value, given by the server, but ensure it is part of the
+          // response
+          if (res.body.last_modified) {
+            res.body.last_modified = 'date';
+          }
+        })
+        .expect(200, {
+          content: content,
+          uuid: EXISTING_DOCUMENT_UUID,
+          last_modified: 'date',
+          version: 124
+        }, done);
+    });
+  });
+});
--- app/server.js
+++ app/server.js
@@ -1,23 +1,88 @@
 var express = require('express');
-var path = require('path');
 var compression = require('compression');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
 
 var app = express();
+var api = express.Router();
 
+// config
 var static_path = path.join(__dirname, '/../build');
+var data_dir    = process.env.MONOD_DATA_DIR || path.join(__dirname, '/../data');
 
 app.set('port', process.env.PORT || 3000);
 
+// middlewares
 app.use(compression());
 app.use(express.static(static_path));
+app.use(bodyParser.json());
+app.use(api);
+
+const isValidId = (uuid) => {
+  return /[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/.test(uuid);
+};
 
 // Match UUIDs
-app.get('/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}', function (req, res) {
+app.get('/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}', (req, res) => {
   res.sendFile('index.html', {
     root: static_path
   });
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Running at localhost:' + app.get('port'));
+// API
+api.get('/documents/:uuid', (req, res) => {
+  var uuid = req.params.uuid;
+
+  // request validation
+  if (!isValidId(uuid)) {
+    return res.status(400).json();
+  }
+
+  fs.readFile(path.join(data_dir, uuid), (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).json();
+    }
+
+    return res.json(JSON.parse(data));
+  });
 });
+
+api.put('/documents/:uuid', (req, res) => {
+  const uuid = req.params.uuid;
+
+  // request validation
+  if (!isValidId(uuid) || !req.body.content) {
+    return res.status(400).json();
+  }
+
+  const filename = path.join(data_dir, uuid);
+
+  fs.readFile(filename, (err, data) => {
+    const isNew = err ? true : false;
+    const document = isNew ? { version: 0 } : JSON.parse(data);
+
+    document.uuid          = uuid;
+    document.content       = req.body.content;
+    document.last_modified = new Date();
+    document.version       = document.version + 1;
+
+    fs.writeFile(filename, JSON.stringify(document), (err) => {
+      if (err) {
+        return res.status(500).json();
+      }
+
+      return res.status(isNew ? 201 : 200).json(document);
+    });
+  });
+});
+
+// Listen only when doing: `node app/server.js`
+if (require.main === module) {
+  app.listen(app.get('port'), () => {
+    console.log('Running at localhost:' + app.get('port'));
+  });
+}
+
+module.exports = app;
