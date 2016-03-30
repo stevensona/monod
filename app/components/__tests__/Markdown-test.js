052d048ed87d1e393fc4a2a684d010326d31750f
Julien Maupetit <julien@tailordev.fr>
2016-03-30 17:24:32 +0200

    Fix Markdown component tests
    This is an ugly thing.

--- app/components/__tests__/Markdown-test.js
+++ app/components/__tests__/Markdown-test.js
@@ -18,34 +18,43 @@ describe('<Markdown />', () => {
 
   it('renders a CodeMirror block', () => {
     const wrapper = mount(<Markdown raw={""} onChange={() => {}} doUpdatePosition={() => {}} />);
-    expect(wrapper.render().find('.CodeMirror')).to.have.length(1);
+
+    setTimeout(() => {
+      expect(wrapper.render().find('.CodeMirror')).to.have.length(1);
+    }, 5);
   });
 
   it('calls onChange when a default value is provided', () => {
     const spy = sinon.spy();
     mount(<Markdown raw={"foo"} onChange={spy} doUpdatePosition={() => {}} />);
 
-    expect(spy.calledOnce).to.be.true;
+    setTimeout(() => {
+      expect(spy.calledOnce).to.be.true;
+    }, 5);
   });
 
   it('calls onChange when a text is entered', () => {
     const spy = sinon.spy();
     const wrapper = mount(<Markdown raw={""} onChange={spy} doUpdatePosition={() => {}} />);
 
-    wrapper.instance().getCodeMirror().getDoc().setValue('hello');
-
-    expect(spy.calledOnce).to.be.true;
+    setTimeout(() => {
+      wrapper.instance().getCodeMirror().getDoc().setValue('hello');
+      expect(spy.calledOnce).to.be.true;
+    }, 5);
   });
 
   it('calls doUpdatePosition when scrolling', () => {
     const spy = sinon.spy();
     const wrapper = mount(<Markdown raw={""} onChange={() => {}} doUpdatePosition={spy} />);
-    const codeMirror = wrapper.instance().getCodeMirror();
 
-    // CodeMirror won't scroll if it has no value
-    codeMirror.getDoc().setValue('hello world!');
-    codeMirror.scrollTo(0, 10);
+    setTimeout(() => {
+      const codeMirror = wrapper.instance().getCodeMirror();
 
-    expect(spy.called).to.be.true;
+      // CodeMirror won't scroll if it has no value
+      codeMirror.getDoc().setValue('hello world!');
+      codeMirror.scrollTo(0, 10);
+
+      expect(spy.called).to.be.true;
+    }, 5);
   });
 });
