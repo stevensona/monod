b8ec66e7df7088bb790180f5afe169f4c8e54eac
William Durand <will+git@drnd.me>
2016-03-31 11:52:32 +0200

    Add Sync component

---
+++ app/components/Sync.jsx
@@ -0,0 +1,92 @@
+import React, { Component, PropTypes } from 'react';
+import { Events } from '../Store';
+import moment from 'moment';
+
+const DEFAULT_DURATION = 5;
+
+export default class Sync extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      date: null,
+      duration: DEFAULT_DURATION,
+      counter: DEFAULT_DURATION,
+      offline: false
+    };
+  }
+
+  componentWillMount() {
+    this.interval = setInterval(() => {
+      let counter = this.state.counter;
+
+      if (0 < counter) {
+        counter--;
+        this.setState({ counter: counter });
+
+        if (0 === counter) {
+          this.setState({
+            counter: this.state.duration
+          });
+          this.context.controller.dispatch('action:sync');
+        }
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  componentDidMount() {
+    this.context.controller.on(Events.SYNCHRONIZE, (state) => {
+      this.setState({
+        date: state.date,
+        duration: DEFAULT_DURATION
+      });
+    });
+
+    this.context.controller.on(Events.APP_IS_ONLINE, () => {
+      this.setState({
+        duration: DEFAULT_DURATION,
+        offline: false
+      });
+    });
+
+    this.context.controller.on(Events.APP_IS_OFFLINE, () => {
+      this.setState({
+        duration: Math.round(this.state.duration * 2),
+        offline: true
+      });
+    });
+  }
+
+  render() {
+    let title = 'No Internet connection';
+
+    if (false === this.state.offline) {
+      title = [
+        'Connected to the Internets™ ',
+        '(Last sync: ',
+        moment(this.state.date).fromNow(),
+        ')'
+      ].join('')
+    }
+
+    return (
+      <div className="sync">
+        <span className="status">
+          <i
+            title={title}
+            className={this.state.offline ? 'fa fa-toggle-off' : 'fa fa-toggle-on'}
+          ></i>
+          {this.state.offline ? <span>&nbsp;Retrying in {this.state.counter} seconds</span> : ''}
+        </span>
+      </div>
+    );
+  }
+}
+
+Sync.contextTypes = {
+  controller: PropTypes.object.isRequired
+};
