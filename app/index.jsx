36236797246ca6b8ea3d502ec998b97cbfd0d682
William Durand <will+git@drnd.me>
2016-03-31 11:53:52 +0200

    Update webpack HTML template to add API endpoint

--- app/index.jsx
+++ app/index.jsx
@@ -8,11 +8,12 @@ import { EventEmitter } from 'events';
 import Store from './Store';
 import Controller from './Controller';
 
-const appElement = document.getElementById('app');
-const appVersion = appElement.getAttribute('data-app-version');
+const appElement  = document.getElementById('app');
+const appVersion  = appElement.getAttribute('data-app-version');
+const apiEndpoint = appElement.getAttribute('data-api-endpoint');
 
 const events     = new EventEmitter();
-const store      = new Store('documents', events);
+const store      = new Store('documents', events, apiEndpoint);
 const controller = new Controller({ store }, events);
 
 require('offline-plugin/runtime').install();
--- webpack.config.js
+++ webpack.config.js
@@ -118,6 +118,7 @@ const common = {
             // Favicon generated with http://realfavicongenerator.net
             favicon: 'app/favicon.ico',
             version: VERSION.substring(0, 7),
+            apiEndpoint: TARGET === 'dev' ? 'http://localhost:3000' : '',
             // Main "div" `id`
             appMountId: 'app',
             // No need to inject assets in the given template as it is handled
