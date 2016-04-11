0c91ab19ff0ce78ebc98b944ea84aaa4f0bb24b8
Julien Maupetit <julien@tailordev.fr>
2016-04-11 17:20:17 +0200

    Add message box close button and event handler

--- app/components/MessageBox.jsx
+++ app/components/MessageBox.jsx
@@ -5,11 +5,18 @@ const { object } = PropTypes;
 
 export default class MessageBox extends Component {
 
+  handleClose() {
+    console.log('handleClose:', this.refs.msgBox);
+  }
+
   render() {
     if (this.props.message && this.props.message.content) {
       return (
-        <div className={'message-box ' + this.props.message.type}>
+        <div className={'message-box ' + this.props.message.type} ref="msgBox">
           <p>{this.props.message.content}</p>
+          <button className="close-button" onClick={this.handleClose.bind(this)}>
+            <span>&times;</span>
+          </button>
         </div>
       );
     }
