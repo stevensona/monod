086de2523849ea94eeabf26401856f28c4dc4e87
William Durand <will+git@drnd.me>
2016-06-25 16:51:00 +0200

    Add Reveal.js as a new template

---
+++ app/components/templates/Slidedeck.jsx
@@ -0,0 +1,58 @@
+/* eslint one-var: 0 */
+import React from 'react';
+import BaseTemplate from './Base';
+import Reveal from 'reveal.js';
+
+import '../../scss/reveal.css';
+import '../../scss/reveal-theme.css';
+
+/**
+ * Slidedeck template
+ */
+export default class Slidedeck extends BaseTemplate {
+
+  getDefaultData() {
+    return {};
+  }
+
+  componentDidMount() {
+    Reveal.initialize({
+      embedded: true,
+      fragments: false,
+      slideNumber: true,
+      progress: false,
+    });
+  }
+
+  render() {
+    const sections = [];
+    let itemsInSection = [];
+    this.props.content.forEach((c) => {
+      if ('preview-loader' === c.key) {
+        return;
+      }
+
+      if ('---' === c.props.chunk[0].markup) {
+        if (itemsInSection.length > 0) {
+          sections.push(itemsInSection);
+          itemsInSection = [];
+        }
+      } else {
+        itemsInSection.push(c);
+      }
+    });
+    sections.push(itemsInSection);
+
+    return (
+      <div className="reveal">
+        <div className="slides">
+          {sections.map((section) =>
+            <section>
+              {section}
+            </section>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
