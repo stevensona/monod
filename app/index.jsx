3148667c7105a94fa5467e6f3bafd315cb273f37
William Durand <will+git@drnd.me>
2016-08-10 15:27:30 +0200

    Install/update libs/config files

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { EventEmitter } from 'events';
-import localforage from 'localforage';
+import { Provider } from 'react-redux';
 
 import './scss/main.scss';
 
 import App from './components/App.jsx';
-import Store from './Store';
-import Controller from './Controller';
+import configureStore from './store/configureStore';
 
 const appElement = document.getElementById('app');
 const appVersion = appElement.getAttribute('data-app-version');
 const apiEndpoint = appElement.getAttribute('data-api-endpoint');
 
-const events = new EventEmitter();
-const store = new Store('documents', events, apiEndpoint, localforage);
-const controller = new Controller({ store }, events);
+const store = configureStore();
 
-require('offline-plugin/runtime').install();
-
-ReactDOM.render(<App version={appVersion} controller={controller} />, appElement);
+//require('offline-plugin/runtime').install();
+ReactDOM.render(
+  <Provider store={store}>
+    <App version={appVersion} controller={controller} />
+  </Provider>,
+  appElement
+);
--- server.js
+++ server.js
@@ -1,87 +1,111 @@
-const express = require('express');
-const compression = require('compression');
-const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs');
+if ('production' === process.env.NODE_ENV) {
+  const express = require('express');
+  const compression = require('compression');
+  const bodyParser = require('body-parser');
+  const path = require('path');
+  const fs = require('fs');
 
-const app = express();
-const api = express.Router();
+  const app = express();
+  const api = express.Router();
 
-// config
-const staticPath = path.join(__dirname, '/build');
-const dataDir = process.env.MONOD_DATA_DIR || path.join(__dirname, '/data');
+  // config
+  const staticPath = path.join(__dirname, '/build');
+  const dataDir = process.env.MONOD_DATA_DIR || path.join(__dirname, '/data');
 
-app.set('port', process.env.PORT || 3000);
-app.set('etag', false);
+  app.set('port', process.env.PORT || 3000);
+  app.set('etag', false);
 
-// middlewares
-app.use(compression());
-app.use(express.static(staticPath));
-app.use(bodyParser.json());
-app.use(api);
+  // middlewares
+  app.use(compression());
+  app.use(express.static(staticPath));
+  app.use(bodyParser.json());
+  app.use(api);
 
-const isValidId = (uuid) => {
-  return /[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/.test(uuid);
-};
+  const isValidId = (uuid) => {
+    return /[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/.test(uuid);
+  };
 
-// Match UUIDs
-app.get('/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}', (req, res) => {
-  res.sendFile('index.html', {
-    root: staticPath
-  });
-});
-
-// API
-api.get('/documents/:uuid', (req, res) => {
-  const uuid = req.params.uuid;
-
-  // request validation
-  if (!isValidId(uuid)) {
-    return res.status(400).json();
-  }
-
-  return fs.readFile(path.join(dataDir, uuid), (err, data) => {
-    if (err) {
-      return res.status(404).json();
-    }
-
-    return res.json(JSON.parse(data));
-  });
-});
-
-api.put('/documents/:uuid', (req, res) => {
-  const uuid = req.params.uuid;
-
-  // request validation
-  if (!isValidId(uuid) || !req.body.content) {
-    return res.status(400).json();
-  }
-
-  const filename = path.join(dataDir, uuid);
-
-  return fs.readFile(filename, (readErr, data) => {
-    const document = readErr ? {} : JSON.parse(data);
-
-    document.uuid = uuid;
-    document.content = req.body.content;
-    document.last_modified = Date.now();
-    document.template = req.body.template || '';
-
-    fs.writeFile(filename, JSON.stringify(document), (err) => {
-      if (err) {
-        return res.status(500).json();
-      }
-
-      return res.status(readErr ? 201 : 200).json(document);
+  // Match UUIDs
+  app.get('/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}', (req, res) => {
+    res.sendFile('index.html', {
+      root: staticPath
     });
   });
-});
 
-// Listen only when doing: `node app/server.js`
-if (require.main === module) {
-  app.listen(app.get('port'), () => {
-    console.log(`Running at localhost: ${app.get('port')}`);
+  // API
+  api.get('/documents/:uuid', (req, res) => {
+    const uuid = req.params.uuid;
+
+    // request validation
+    if (!isValidId(uuid)) {
+      return res.status(400).json();
+    }
+
+    return fs.readFile(path.join(dataDir, uuid), (err, data) => {
+      if (err) {
+        return res.status(404).json();
+      }
+
+      return res.json(JSON.parse(data));
+    });
+  });
+
+  api.put('/documents/:uuid', (req, res) => {
+    const uuid = req.params.uuid;
+
+    // request validation
+    if (!isValidId(uuid) || !req.body.content) {
+      return res.status(400).json();
+    }
+
+    const filename = path.join(dataDir, uuid);
+
+    return fs.readFile(filename, (readErr, data) => {
+      const document = readErr ? {} : JSON.parse(data);
+
+      document.uuid = uuid;
+      document.content = req.body.content;
+      document.last_modified = Date.now();
+      document.template = req.body.template || '';
+
+      fs.writeFile(filename, JSON.stringify(document), (err) => {
+        if (err) {
+          return res.status(500).json();
+        }
+
+        return res.status(readErr ? 201 : 200).json(document);
+      });
+    });
+  });
+
+  // Listen only when doing: `node app/server.js`
+  if (require.main === module) {
+    app.listen(app.get('port'), () => {
+      console.log(`API backend running at http://localhost:${app.get('port')}`);
+    });
+  }
+
+  module.exports = app;
+} else {
+  const port = 4000;
+  const webpack = require('webpack');
+  const WebpackDevServer = require('webpack-dev-server');
+  const config = require('./webpack.config');
+
+  new WebpackDevServer(webpack(config), {
+    hot: true,
+    historyApiFallback: true,
+    publicPath: '/',
+    proxy: {
+      '/documents/*': {
+        target: 'http://localhost:3000',
+      }
+    }
+  }).listen(port, 'localhost', function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log(`Development server running at http://localhost:${port}`);
   });
 }
-
-module.exports = app;
