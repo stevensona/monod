e876f28ba183bdb5872b80b88b30a873b86e366b
William Durand <will+git@drnd.me>
2016-08-17 13:13:34 +0200

    Add a few more tests

--- app/components/Sync/__tests__/Sync-test.js
+++ app/components/Sync/__tests__/Sync-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import Sync from '../presenter';
 import config from '../../../config';
@@ -41,5 +42,39 @@ describe('<Sync />', () => {
     wrapper.setState({ offline: true });
 
     expect(wrapper.html()).to.contain(config.SYNC_OFFLINE_MESSAGE);
+  });
+
+  it('calls onRequestSync prop and increase duration by 1.5 when app is offline', () => {
+    const spy = sinon.spy();
+    const wrapper = mount(
+      <Sync
+        offline
+        onRequestSync={spy}
+      />
+    );
+
+    // force state + call to hit counter == 0
+    wrapper.setState({ duration: 10, counter: 1 });
+    wrapper.instance().counter();
+
+    expect(spy.called).to.be.true;
+    expect(wrapper.state('duration')).to.equal(15);
+  });
+
+  it('calls onRequestSync prop and reset counter when app is online', () => {
+    const spy = sinon.spy();
+    const wrapper = mount(
+      <Sync
+        offline={false}
+        onRequestSync={spy}
+      />
+    );
+
+    // force state + call to hit counter == 0
+    wrapper.setState({ duration: 10, counter: 1 });
+    wrapper.instance().counter();
+
+    expect(spy.called).to.be.true;
+    expect(wrapper.state('duration')).to.equal(config.SYNC_COUNTER_DURATION);
   });
 });
