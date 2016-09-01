8988c4d2904405c1d4c624d22ce2f14e803ae977
Julien Maupetit <julien@tailordev.fr>
2016-09-01 17:27:52 +0200

    Fix fold marker style

--- app/components/Markdown/index.jsx
+++ app/components/Markdown/index.jsx
@@ -4,7 +4,6 @@ import Dropzone from 'react-dropzone';
 
 import 'codemirror/mode/gfm/gfm';
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/addon/fold/foldgutter.css';
 import 'codemirror/addon/fold/foldcode';
 import 'codemirror/addon/fold/foldgutter';
 import './fold';
